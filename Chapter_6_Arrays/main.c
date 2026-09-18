5a2a139d60ce02000b35e80544b212b1f0a05863
eduardoreyes007351208 <lalosoccer307@gmail.com>
2026-09-18 07:02:53 -0700

    feat: Practiced more in Array Chapter 6

--- Chapter_6_Arrays/main.c
+++ Chapter_6_Arrays/main.c
@@ -3,6 +3,13 @@
 int main (void) {
 
     float f[4];
+    int x[12];
+    int a[5] = {22, 33, 55};
+    int row, col;
+    int b[2][5] = {
+        {1, 2, 3, 4, 5},
+        {6, 7, 8, 9, 0}
+    };
 
     f[0] = 3.14;
     f[1] = 6.28;
@@ -11,6 +18,21 @@ int main (void) {
 
     for(int i = 0; i < 4; i++) {
         printf("Float at index %d is %f\n", i, f[i]);
+    }
+
+    printf("%zu\n", sizeof x);
+    printf("%zu\n", sizeof(int));
+
+    printf("%zu\n", sizeof x / sizeof(int));
+
+    for(int i = 0; i < 5; i++) {
+        printf("%d\n", a[i]);
+    }
+
+    for(row = 0; row < 2; row++) {
+        for(col = 0; col < 5; col++) {
+            printf("(%d, %d) = %d\n", row, col, b[row][col]);
+        }
     }
 
 }
