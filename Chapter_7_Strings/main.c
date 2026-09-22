a7bf9a9429c21bac03031e4b85c358a8e36f79d8
eduardoreyes007351208 <lalosoccer307@gmail.com>
2026-09-21 18:29:06 -0700

    feat: Practice more with strings
    Learn different ways of initializing strings,
    how to modify and count lenght,
    how to copy strings

--- Chapter_7_Strings/main.c
+++ Chapter_7_Strings/main.c
@@ -1,15 +1,65 @@
 #include <stdio.h>
+#include <string.h>
 
-int main (void) {
-    
-    char *s = "string";
+int my_strLen (char *s);
+
+int main(void)
+{
+
+    char *s = "Canta y no llores";
     char string[14] = "Hello, World!";
     char stringS[] = "Hello World!";
+    char *t;
+    char z[100];
 
     printf("%s\n", s);
 
-    for (int i = 0; i < 13; i++) {
+    // Printing all char from array
+    for (int i = 0; i < 13; i++)
+    {
         printf("%c", stringS[i]);
     }
 
+    printf("%zu\n", strlen(string));
+
+    // testing a string length counter
+    for (int i = 0; i < 13; i++)
+    {
+        if (stringS[i] == '\0')
+        {
+            printf("Null\n");
+        }
+        else
+        {
+            printf("%c\n", stringS[i]);
+        }
+    }
+
+
+    // Practicing the custom string length function
+    int strLength = my_strLen(s);
+    printf("Length of string s (%s) is %d\n", s, strLength);
+
+    // string copying practice
+    t = stringS;
+
+    printf("string stringS is %s\n", stringS);
+    t[0] = 'z';
+    printf("string stringS is %s\n", stringS);
+
+    strcpy(z, string);
+
+    z[0] = 'z';
+    printf("string string is %s\n", string);
+    printf("string z is %s\n", z);
+}
+
+int my_strLen (char *s) {
+    int count = 0;
+
+    while (s[count] != '\0') {
+        count++;
+    }
+
+    return count;
 }
